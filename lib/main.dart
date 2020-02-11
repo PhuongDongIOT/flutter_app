91905bd07ff821e00280b4b41be3f22540d0e87b
dongpp <phuongdongIOT@gmail.com>
2020-02-11 22:47:47 +0700

    ADD thêm color

--- lib/main.dart
+++ lib/main.dart
@@ -102,7 +102,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Startup Name Generator',
-        home: RandomWords(),
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),
+      home: RandomWords(),
     );
   }
 }
